2c3c343bc1fd2d99aa91472edb0a646b5e6b58b4
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-04 18:52:35 +0530

    First program execution in JS
    First program execution in JS

---
+++ personal_info.js
@@ -0,0 +1,7 @@
+
+console.log("Name: Suresh Choudhari");
+console.log("Hobies: 1.Reading Books, 2.Watching Movie, 3.Traveling");
+console.info("Strength: 1.Determination, 2.Patience, 3.Dedication");
+console.warn("Weakness: 1.Self-criticism, 2.Being straight forward, 3.Does not like conflict")
+console.log("30+50");
+console.log("49*479");
