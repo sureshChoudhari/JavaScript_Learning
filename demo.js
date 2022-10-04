2dbb9ced81562f4c8a5585692c8a5819bf8faeac
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-04 08:38:10 +0530

    write demo code in demo.js
    write demo code in demo.js

---
+++ demo.js
@@ -0,0 +1,6 @@
+
+console.log("Hello");
+console.log('My name is Suresh');
+console.info("This is Info");// info means Information
+console.warn("I am not that guy");//warn means Warning
+console.error("Opps failed to execute");
