47082f5ad7275f564e510409fdca62f63b9952fa
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-06 18:20:03 +0530

    variable assign in 01_variable_assign.js
    variable assign in 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,16 @@
+var myFullName;
+console.log(myFullName);
+myFullName= "Suresh Choudhari";
+console.log(myFullName);
+myFullName="Suresh Udhavrao Choudhari";
+console.log(myFullName);
+var myLove="I love only JS"
+console.log(myLove);
+var firstName="Suresh";
+console.log(firstName),lastName="Choudhari",age="30",address="At Post Padalsingi, Tq Georai, Dist Beed",pin_code="431143", gender="Male",is_married="Unmarried";
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pin_code);
+console.log(gender);
+console.log(is_married);
