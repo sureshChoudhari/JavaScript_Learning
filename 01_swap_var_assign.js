c6229d6755cca972f891e2d9033290df6ee45201
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-06 18:51:49 +0530

    swap two and three variable in 01-swap_var_assign.js
    swap two and three variable in 01-swap_var_assign.js

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,18 @@
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("Before swapping");
+console.log(sweety , cutie);
+var temp="sweety";
+sweety=cutie;
+cutie=temp;
+console.log("After swapping");
+console.log(sweety , cutie);
+var num1="100",num2="200",num3="300";
+console.log("Before swapping");
+console.log(num1,num2,num3);
+var file=num1;
+num1=num2;
+num2=num3;
+num3=file;
+console.log("After swapping");
+console.log(num1,num2,num3);
--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -6,8 +6,8 @@ myFullName="Suresh Udhavrao Choudhari";
 console.log(myFullName);
 var myLove="I love only JS"
 console.log(myLove);
-var firstName="Suresh";
-console.log(firstName),lastName="Choudhari",age="30",address="At Post Padalsingi, Tq Georai, Dist Beed",pin_code="431143", gender="Male",is_married="Unmarried";
+var firstName="Suresh",lastName="Choudhari",age="30",address="At Post Padalsingi, Tq Georai, Dist Beed",pin_code="431143", gender="Male",is_married="Unmarried";
+console.log(firstName)
 console.log(lastName);
 console.log(age);
 console.log(address);
