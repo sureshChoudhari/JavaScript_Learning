cc49c5c6a78955f660a7a69fe720f6e2aa80e357
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-07 15:39:38 +0530

    add typeof Sting, Number, Boolean in assignment2.js
    add typeof Sting, Number, Boolean in assignment2.js

---
+++ 02_assignment2.js
@@ -0,0 +1,12 @@
+
+console.log("Question:1)");
+var PIN=431143;
+var myName="Suresh";
+var is_married= false;
+console.log("PIN :" ,PIN ,"Type :", typeof PIN);
+console.log("Name :" ,myName,"Type :", typeof myName);
+console.log("Married Status:" ,is_married, "Type :" ,typeof is_married);
+console.log("-----------------------------------------");
+console.log("Question:2)");
+console.log("Null: It is the intentional absence of the value. It is one of the primitive values of JavaScript.");
+console.log("Undefined: It means the value does not exist in the compiler.");
