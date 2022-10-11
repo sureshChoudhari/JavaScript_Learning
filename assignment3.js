99e207a638b55620afc5dd87231bb138536e52b2
sureshChoudhari <choudharisuresh@gmail.com>
2022-10-11 22:07:00 +0530

    assignment 1 completed
    assignment 1 completed

---
+++ assignment3.js
@@ -0,0 +1,14 @@
+console.log("===Question 1)===");
+console.log(`My dream company is "Tata Consultancy Services" `);
+
+console.log("===Question 2)===");
+console.log("===Question 2.1)===");
+var hobby1 = "1) Reading Books";
+var hobby2 = "2) Watching Cricket";
+var hobby3 = "3) Listening Music";
+console.log(hobby1);
+console.log(hobby2);
+console.log(hobby3);
+console.log("===Question 2.2)===");
+result = hobby1.concat(hobby2).concat(hobby3);
+console.log(result);
